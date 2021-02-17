a2cfe7760f0290523ce818318655f10b9251da7e
Nilkumar Patel <nilkumarpatel48@gmail.com>
2021-02-17 16:38:33 -0500

    Final Test

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <GameBoard style="" />
+        <GameBoard style="Error" />
       </View>
     )
   }
